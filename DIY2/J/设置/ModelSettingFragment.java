cc8cef1c17c71399c6de755dab31d89984512039
zxyadc <101701125+zxyadc@users.noreply.github.com>
2023-01-09 22:42:59 +0800

    Update ModelSettingFragment.java

--- DIY2/J/设置/ModelSettingFragment.java
+++ DIY2/J/设置/ModelSettingFragment.java
@@ -237,7 +237,7 @@ public class ModelSettingFragment extends BaseLazyFragment {
                 ArrayList<String> history = Hawk.get(HawkConfig.API_HISTORY, new ArrayList<String>());
                 if (history.isEmpty())
                     return;
-                String current = Hawk.get(HawkConfig.API_URL, "");
+                String current = Hawk.get(HawkConfig.API_URL, "这是什么/n就的撒");
                 int idx = 0;
                 if (history.contains(current))
                     idx = history.indexOf(current);
